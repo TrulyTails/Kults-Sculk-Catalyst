8dd21fb60ae56bee2b1caced65032fe44bc0b3ae
tails <tails0009@gmail.com>
2024-06-11 03:16:02 -0400

    first commit

---
+++ src/main/java/net/kultist/ksc/KultsSculkCatalystClient.java
@@ -0,0 +1,10 @@
+package net.kultist.ksc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KultsSculkCatalystClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kultist/ksc/KultsSculkCatalystDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kultist.ksc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KultsSculkCatalystDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
